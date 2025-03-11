60a9e4e96dfce0f20560051d22287f0783edb831
gabriel-cruz678 <154607281+gabriel-cruz678@users.noreply.github.com>
2025-03-11 12:40:00 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,15 @@
+USER = 1
+HEADERS = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/vnd.tasksmanager.v2'
+}
+
+DATA = {
+    "name": "Naldo Teste",
+    "job": "leader"
+}
+
+UPDATE_USER = {
+    "name": "Naldo Teste Updated",
+    "job": "leader"
+}
